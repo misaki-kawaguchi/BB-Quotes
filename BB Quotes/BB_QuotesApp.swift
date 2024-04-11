e475bb7ad47dad21afeedf3357a250f3126023cc
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-12 08:43:18 +0900

    first commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by 川口美咲 on 2024/04/12.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
