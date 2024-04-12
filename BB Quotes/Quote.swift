571d8c60d024854a7c8ffd903a2c780d783517cb
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-04-12 09:15:39 +0900

    Quoteモデル・Characterモデルの作成 (#2)

---
+++ BB Quotes/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  BB Quotes
+//
+//  Created by 川口美咲 on 2024/04/12.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+    let production: String
+}
