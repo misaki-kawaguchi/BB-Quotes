8b5d658cabefb1d0444b0e83f318a86518fed701
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-04-14 10:42:52 +0900

    Lesson 9 - Better Call Saul View (#9)
    * AssetsにColorを追加
    
    * 背景画像によってボタンの色を変更できるようにする・リファクタリング
    
    * ボタンを下部に固定する

--- BB Quotes/QuoteView.swift
+++ BB Quotes/QuoteView.swift
@@ -15,55 +15,60 @@ struct QuoteView: View {
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                Image(show.lowercased().filter {$0 != " "})
+                Image(show.lowerNoSpaces)
                     .resizable()
                     .frame(
                         width: geo.size.width * 2.7,
                         height: geo.size.height * 1.2
                     )
+                
                 VStack {
-                    Spacer(minLength: 120)
-                    
-                    switch viewModel.status {
-                    case .success(let data) :
-                        Text("\"\(data.quote.quote)\"")
-                            .minimumScaleFactor(0.5)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(.black.opacity(0.5))
-                            .cornerRadius(25)
-                            .padding(.horizontal)
+                    VStack {
+                        Spacer(minLength: 120)
                         
-                        ZStack(alignment: .bottom) {
-                            AsyncImage(url: data.character.images[0]) { image in
-                                image.resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
+                        switch viewModel.status {
+                        case .success(let data) :
+                            Text("\"\(data.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .cornerRadius(25)
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: data.character.images[0]) { image in
+                                    image.resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(
+                                    width: geo.size.width / 1.1,
+                                    height: geo.size.height / 1.8
+                                )
+                                
+                                Text("\(data.quote.character)")
+                                    .foregroundColor(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
                             }
                             .frame(
                                 width: geo.size.width / 1.1,
                                 height: geo.size.height / 1.8
                             )
+                            .cornerRadius(80)
                             
-                            Text("\(data.quote.character)")
-                                .foregroundColor(.white)
-                                .padding(10)
-                                .frame(maxWidth: .infinity)
-                                .background(.ultraThinMaterial)
+                        case .fetching:
+                            ProgressView()
+                            
+                        default:
+                            EmptyView()
                         }
-                        .frame(
-                            width: geo.size.width / 1.1,
-                            height: geo.size.height / 1.8
-                        )
-                        .cornerRadius(80)
                         
-                    case .fetching:
-                        ProgressView()
-                        
-                    default:
-                        EmptyView()
+                        Spacer()
                     }
                     
                     Button {
@@ -75,12 +80,12 @@ struct QuoteView: View {
                             .font(.title)
                             .foregroundColor(.white)
                             .padding()
-                            .background(.breakingBadGreen)
+                            .background(Color("\(show.noSpaces)Button"))
                             .cornerRadius(7)
-                            .shadow(color: .breakingBadYellow, radius: 2)
+                            .shadow(color:Color("\(show.noSpaces)Shadow"), radius: 2)
                     }
                     
-                    Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                    Spacer(minLength: 140)
                 }
                 .frame(width: geo.size.width)
             }
@@ -94,6 +99,6 @@ struct QuoteView: View {
 }
 
 #Preview {
-    QuoteView(show: "Breaking Bad")
+    QuoteView(show: Constants.bcsName)
         .preferredColorScheme(.dark)
 }
